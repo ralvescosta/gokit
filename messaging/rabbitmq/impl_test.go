9e83e4b168f5240ddc1084658f3afb8bc0b7067c
ralvescsota <rafael.rac.mg@gmail.com>
2022-07-05 05:30:41 -0300

    feat: unit test

--- messaging/rabbitmq/impl_test.go
+++ messaging/rabbitmq/impl_test.go
@@ -96,7 +96,7 @@ func (s *RabbitMQMessagingSuiteTest) TestDeclareErr() {
 	s.Nil(s.messaging.topologies)
 }
 
-func (s *RabbitMQMessagingSuiteTest) TestABind() {
+func (s *RabbitMQMessagingSuiteTest) TestBind() {
 	tp := &Topology{
 		Exchange: &ExchangeOpts{
 			Name: "",
@@ -112,4 +112,187 @@ func (s *RabbitMQMessagingSuiteTest) TestABind() {
 
 	s.NotNil(tp.deadLetter)
 	s.NotNil(tp.delayed)
+}
+
+func (s *RabbitMQMessagingSuiteTest) TestApplyBinds() {
+	tp := &Topology{
+		Exchange: &ExchangeOpts{
+			Name: "",
+			Type: DIRECT_EXCHANGE,
+		},
+		Queue: &QueueOpts{
+			WithDeadLatter: true,
+			Retryable:      &Retry{},
+		},
+	}
+
+	s.messaging.Declare(tp).ApplyBinds()
+
+	s.NotNil(tp.deadLetter)
+	s.NotNil(tp.delayed)
+}
+
+func (s *RabbitMQMessagingSuiteTest) TestApplyBindsErr() {
+	s.messaging.Err = errors.New("some error")
+	tp := &Topology{}
+
+	s.messaging.Declare(tp).ApplyBinds()
+
+	s.Nil(tp.deadLetter)
+	s.Nil(tp.delayed)
+}
+
+func (s *RabbitMQMessagingSuiteTest) TestBuild() {
+	tp := &Topology{
+		Exchange: &ExchangeOpts{
+			Name:     "exchange",
+			Type:     DIRECT_EXCHANGE,
+			Bindings: []string{"to-bind"},
+		},
+		Queue: &QueueOpts{
+			Name:           "queue",
+			WithDeadLatter: true,
+			Retryable: &Retry{
+				NumberOfRetry: 3,
+				DelayBetween:  10,
+			},
+		},
+	}
+
+	msg := s.messaging.Declare(tp).ApplyBinds()
+
+	s.amqpChannel.
+		On("ExchangeDeclare", tp.Exchange.Name, string(tp.Exchange.Type), true, false, false, false, amqp.Table(nil)).
+		Return(nil).
+		Once()
+	s.amqpChannel.
+		On("ExchangeDeclare", tp.delayed.ExchangeName, string(DELAY_EXCHANGE), true, false, false, false, amqp.Table{
+			"x-delayed-type": "direct",
+		}).
+		Return(nil).
+		Once()
+
+	s.amqpChannel.
+		On("ExchangeBind", tp.Exchange.Bindings[0], s.messaging.newRoutingKey(tp.Exchange.Name, tp.Exchange.Bindings[0]), tp.Exchange.Name, false, amqp.Table(nil)).
+		Return(nil)
+
+	s.amqpChannel.
+		On("QueueDeclare", tp.deadLetter.QueueName, true, false, false, false, amqp.Table(nil)).
+		Return(amqp.Queue{}, nil).
+		Once()
+	s.amqpChannel.
+		On("QueueDeclare", tp.Queue.Name, true, false, false, false, amqp.Table{
+			"x-dead-letter-exchange":    "",
+			"x-dead-letter-routing-key": tp.deadLetter.QueueName,
+		}).
+		Return(amqp.Queue{}, nil).
+		Once()
+
+	s.amqpChannel.
+		On("QueueBind", tp.Queue.Name, tp.Binding.RoutingKey, tp.Exchange.Name, false, amqp.Table(nil)).
+		Return(nil).
+		Once()
+	s.amqpChannel.
+		On("QueueBind", tp.delayed.QueueName, tp.Binding.delayedRoutingKey, tp.delayed.ExchangeName, false, amqp.Table(nil)).
+		Return(nil).
+		Once()
+
+	msg.Build()
+
+	s.amqpChannel.AssertExpectations(s.T())
+}
+
+func (s *RabbitMQMessagingSuiteTest) TestBuildErr() {
+	s.messaging.Err = errors.New("some error")
+	tp := &Topology{}
+
+	_, err := s.messaging.Declare(tp).ApplyBinds().Build()
+
+	s.Error(err)
+}
+
+func (s *RabbitMQMessagingSuiteTest) TestBuildDeclareExchangeErr() {
+	tp := &Topology{
+		Exchange: &ExchangeOpts{
+			Name:     "exchange",
+			Type:     DIRECT_EXCHANGE,
+			Bindings: []string{"to-bind"},
+		},
+		Queue: &QueueOpts{
+			Name:           "queue",
+			WithDeadLatter: true,
+			Retryable: &Retry{
+				NumberOfRetry: 3,
+				DelayBetween:  10,
+			},
+		},
+	}
+
+	msg := s.messaging.Declare(tp).ApplyBinds()
+
+	s.amqpChannel.
+		On("ExchangeDeclare", tp.Exchange.Name, string(tp.Exchange.Type), true, false, false, false, amqp.Table(nil)).
+		Return(errors.New("some error")).
+		Once()
+
+	_, err := msg.Build()
+
+	s.Error(err)
+	s.amqpChannel.AssertExpectations(s.T())
+
+	s.amqpChannel.
+		On("ExchangeDeclare", tp.Exchange.Name, string(tp.Exchange.Type), true, false, false, false, amqp.Table(nil)).
+		Return(nil).
+		Once()
+	s.amqpChannel.
+		On("ExchangeDeclare", tp.delayed.ExchangeName, string(DELAY_EXCHANGE), true, false, false, false, amqp.Table{
+			"x-delayed-type": "direct",
+		}).
+		Return(errors.New("some error")).
+		Once()
+
+	_, err = msg.Build()
+
+	s.Error(err)
+	s.amqpChannel.AssertExpectations(s.T())
+}
+
+func (s *RabbitMQMessagingSuiteTest) TestBuildBindExchangeErr() {
+	tp := &Topology{
+		Exchange: &ExchangeOpts{
+			Name:     "exchange",
+			Type:     DIRECT_EXCHANGE,
+			Bindings: []string{"to-bind"},
+		},
+		Queue: &QueueOpts{
+			Name:           "queue",
+			WithDeadLatter: true,
+			Retryable: &Retry{
+				NumberOfRetry: 3,
+				DelayBetween:  10,
+			},
+		},
+	}
+
+	msg := s.messaging.Declare(tp).ApplyBinds()
+
+	s.amqpChannel.
+		On("ExchangeDeclare", tp.Exchange.Name, string(tp.Exchange.Type), true, false, false, false, amqp.Table(nil)).
+		Return(nil).
+		Once()
+	s.amqpChannel.
+		On("ExchangeDeclare", tp.delayed.ExchangeName, string(DELAY_EXCHANGE), true, false, false, false, amqp.Table{
+			"x-delayed-type": "direct",
+		}).
+		Return(nil).
+		Once()
+
+	s.amqpChannel.
+		On("ExchangeBind", tp.Exchange.Bindings[0], s.messaging.newRoutingKey(tp.Exchange.Name, tp.Exchange.Bindings[0]), tp.Exchange.Name, false, amqp.Table(nil)).
+		Return(errors.New("some error"))
+
+	_, err := msg.Build()
+
+	s.amqpChannel.AssertExpectations(s.T())
+	s.Error(err)
 }
