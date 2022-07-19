5e7085ae852393195772a129a70b4caa0f2ccc4a
ralvescsota <rafael.rac.mg@gmail.com>
2022-07-18 21:45:07 -0300

    feat: telemetry

--- telemetry/go.mod
+++ telemetry/go.mod
@@ -39,7 +39,6 @@ require (
 require (
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.7.0
 	go.opentelemetry.io/otel/sdk v1.8.0
 	go.opentelemetry.io/otel/trace v1.8.0 // indirect
 )
--- telemetry/trace/const.go
+++ telemetry/trace/const.go
@@ -1,9 +1,11 @@
 package trace
 
 const (
-	UNKNOWN_EXPORTER OTLPExporterType = 0
-	GRPC_EXPORTER    OTLPExporterType = 1
-	HTTPS_EXPORTER   OTLPExporterType = 2
+	UNKNOWN_EXPORTER  OTLPExporterType = 0
+	TLS_GRPC_EXPORTER OTLPExporterType = 1
+	GRPC_EXPORTER     OTLPExporterType = 2
+	HTTPS_EXPORTER    OTLPExporterType = 3
+	HTTP_EXPORTER     OTLPExporterType = 4
 
 	OTLP_GZIP_COMPRESSIONS OTLPCompression = "gzip"
 )
