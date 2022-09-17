5cf26364629d24927959c6c50818447d6d110654
ralvescsota <rafael.rac.mg@gmail.com>
2022-09-17 14:16:35 -0300

    feat: envs

--- env/tracing.go
+++ env/tracing.go
@@ -3,6 +3,7 @@ package env
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 const (
@@ -26,6 +27,18 @@ func (c *Config) Tracing() ConfigBuilder {
 
 	c.OTLP_ENDPOINT = os.Getenv(OTLP_ENDPOINT_ENV_KEY)
 	c.OTLP_API_KEY = os.Getenv(OTLP_API_KEY_ENV_KEY)
+	c.JAEGER_SERVICE_NAME = os.Getenv(JAEGER_SERVICE_NAME_KEY)
+	c.JAEGER_AGENT_HOST = os.Getenv(JAEGER_AGENT_HOST_KEY)
+	c.JAEGER_SAMPLER_TYPE = os.Getenv(JAEGER_SAMPLER_TYPE_KEY)
+	if samplerParam := os.Getenv(JAEGER_SAMPLER_PARAM_KEY); samplerParam != "" {
+		c.JAEGER_SAMPLER_PARAM, _ = strconv.Atoi(samplerParam)
+	}
+	if reportLogSpans := os.Getenv(JAEGER_REPORTER_LOG_SPANS_KEY); reportLogSpans != "" {
+		c.JAEGER_REPORTER_LOG_SPANS = reportLogSpans == "true"
+	}
+	if rpcMetrics := os.Getenv(JAEGER_RPC_METRICS_KEY); rpcMetrics != "" {
+		c.JAEGER_RPC_METRICS = rpcMetrics == "true"
+	}
 
 	return c
 }
