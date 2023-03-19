31bbb4b9ed2308ccb20ffeb67214a7869af59cd2
ralvescosta <rafael.rac.mg@gmail.com>
2023-03-19 12:50:54 -0300

    refactor: channel

--- rabbitmq/channel.go
+++ rabbitmq/channel.go
@@ -4,11 +4,11 @@ import "github.com/streadway/amqp"
 
 type (
 	AMQPChannel interface {
-		ExchangeDeclare(name, kind string, durable, autoDelete, internal, noWait bool, args map[string]interface{}) error
-		ExchangeBind(destination, key, source string, noWait bool, args map[string]interface{}) error
-		QueueDeclare(name string, durable, autoDelete, exclusive, noWait bool, args map[string]interface{}) (amqp.Queue, error)
-		QueueBind(name, key, exchange string, noWait bool, args map[string]interface{}) error
-		Consume(queue, consumer string, autoAck, exclusive, noLocal, noWait bool, args map[string]interface{}) (<-chan amqp.Delivery, error)
+		ExchangeDeclare(name, kind string, durable, autoDelete, internal, noWait bool, args amqp.Table) error
+		ExchangeBind(destination, key, source string, noWait bool, args amqp.Table) error
+		QueueDeclare(name string, durable, autoDelete, exclusive, noWait bool, args amqp.Table) (amqp.Queue, error)
+		QueueBind(name, key, exchange string, noWait bool, args amqp.Table) error
+		Consume(queue, consumer string, autoAck, exclusive, noLocal, noWait bool, args amqp.Table) (<-chan amqp.Delivery, error)
 		Publish(exchange, key string, mandatory, immediate bool, msg amqp.Publishing) error
 	}
 )
