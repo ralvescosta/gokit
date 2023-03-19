4b07ef6a61953ec04b1cc1c1dcc95c5cc6552098
ralvescosta <rafael.rac.mg@gmail.com>
2023-03-19 13:02:18 -0300

    refactor: fix consumer

--- rabbitmq/channel.go
+++ rabbitmq/channel.go
@@ -1,6 +1,13 @@
 package rabbitmq
 
-import "github.com/streadway/amqp"
+import (
+	"fmt"
+
+	"github.com/ralvescosta/gokit/env"
+	"github.com/ralvescosta/gokit/logging"
+	"github.com/streadway/amqp"
+	"go.uber.org/zap"
+)
 
 type (
 	AMQPChannel interface {
@@ -12,3 +19,27 @@ type (
 		Publish(exchange, key string, mandatory, immediate bool, msg amqp.Publishing) error
 	}
 )
+
+var dial = func(cfg *env.RabbitMQConfigs) (AMQPConnection, error) {
+	return amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s", cfg.User, cfg.Password, cfg.VHost, cfg.Port))
+}
+
+func NewChannel(cfg *env.RabbitMQConfigs, logger logging.Logger) (AMQPChannel, error) {
+	logger.Debug(LogMessage("connecting to rabbitmq..."))
+	conn, err := dial(cfg)
+	if err != nil {
+		logger.Error(LogMessage("failure to connect to the broker"), zap.Error(err))
+		return nil, err
+	}
+	logger.Debug(LogMessage("connected to rabbitmq"))
+
+	logger.Debug(LogMessage("creating amqp channel..."))
+	ch, err := conn.Channel()
+	if err != nil {
+		logger.Error(LogMessage("failure to establish the channel"), zap.Error(err))
+		return nil, err
+	}
+	logger.Debug(LogMessage("created amqp channel"))
+
+	return ch, nil
+}
