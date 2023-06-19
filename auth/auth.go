ba3aa6eff3e4405f2f5efa9e44c33ffa98c725e7
ralvescosta <rafael.rac.mg@gmail.com>
2023-06-19 08:49:02 -0300

    feat: auth

--- auth/auth.go
+++ auth/auth.go
@@ -1,1 +1,17 @@
 package auth
+
+type (
+	Session struct {
+		Iss     string
+		Sub     string
+		Aud     []string
+		Iat     uint32
+		Exp     uint32
+		Scope   string
+		Session map[string]interface{}
+	}
+
+	TokenManager interface {
+		Validate(token string) (*Session, error)
+	}
+)
