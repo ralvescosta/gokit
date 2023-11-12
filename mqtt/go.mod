3354590851baa3fb22243a96f0faeacd3c4d01e5
ralvescosta <rafael.rac.mg@gmail.com>
2023-11-12 10:21:40 -0300

    feat: mqtt

--- mqtt/go.mod
+++ mqtt/go.mod
@@ -1,3 +1,11 @@
 module github.com/ralvescosta/gokit/mqtt
 
 go 1.21.4
+
+require github.com/eclipse/paho.mqtt.golang v1.4.3
+
+require (
+	github.com/gorilla/websocket v1.5.1 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+)
--- mqtt/mqtt.go
+++ mqtt/mqtt.go
@@ -1,1 +1,20 @@
 package mqtt
+
+import (
+	myQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+func New() {
+	opts := myQTT.NewClientOptions()
+
+	opts.AddBroker("")
+	opts.SetClientID("")
+	opts.SetUsername("")
+	opts.SetPassword("")
+
+	client := myQTT.NewClient(opts)
+
+	client.Connect()
+
+	println(opts)
+}
