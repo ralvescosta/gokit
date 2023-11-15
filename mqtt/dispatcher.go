cea0eec1bdf1e651708029ce5670dc010859004c
ralvescosta <rafael.rac.mg@gmail.com>
2023-11-15 07:37:18 -0300

    feat: mqtt publisher and disptacher

--- mqtt/dispatcher.go
+++ mqtt/dispatcher.go
@@ -1,1 +1,43 @@
 package mqtt
+
+import (
+	"errors"
+
+	myQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/ralvescosta/gokit/logging"
+)
+
+type (
+	Dispatcher interface{}
+
+	dispatcher struct {
+		logger      logging.Logger
+		client      myQTT.Client
+		subscribers map[string]myQTT.MessageHandler
+	}
+)
+
+func NewDispatcher(logger logging.Logger, client myQTT.Client) Dispatcher {
+	return &dispatcher{
+		logger:      logger,
+		client:      client,
+		subscribers: make(map[string]myQTT.MessageHandler),
+	}
+}
+
+func (d *dispatcher) Register(topic string, qos byte, handler myQTT.MessageHandler) error {
+	if topic == "" {
+		return errors.New("")
+	}
+
+	if handler == nil {
+		return errors.New("")
+	}
+
+	//TODO: we need to store the qos to
+	d.subscribers[topic] = handler
+
+	return nil
+}
+
+func (d *dispatcher) ConsumeBlocking() {}
--- mqtt/publisher.go
+++ mqtt/publisher.go
@@ -1,1 +1,32 @@
 package mqtt
+
+import (
+	myQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/ralvescosta/gokit/logging"
+)
+
+type (
+	Publisher interface {
+		Pub(topic string, qos byte, payload any)
+		PubRetained(topic string, qos byte, payload any)
+	}
+
+	publisher struct {
+		logger logging.Logger
+		client myQTT.Client
+	}
+)
+
+func NewPublisher(logger logging.Logger, client myQTT.Client) Publisher {
+	return &publisher{logger, client}
+}
+
+func (p *publisher) Pub(topic string, qos byte, payload any) {
+	p.client.Publish(topic, qos, false, payload)
+	p.logger.Debug(LogMessage("msg published on topic: ", topic))
+}
+
+func (p *publisher) PubRetained(topic string, qos byte, payload any) {
+	p.client.Publish(topic, qos, true, payload)
+	p.logger.Debug(LogMessage("msg published on topic: ", topic))
+}
